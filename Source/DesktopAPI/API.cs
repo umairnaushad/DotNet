e83423d3d3b709159076293f53736ae7bb5ea4cd
Umair Naushad <Umair.Naushad@emaratech.ae>
2019-02-02 23:46:36 +0400

    completed

---
+++ Source/DesktopAPI/API.cs
@@ -0,0 +1,11 @@
+﻿namespace DesktopAPI
+{
+    public class API
+    {
+        private string title;
+        private string version;
+
+        public string Title { get => title; set => title = value; }
+        public string Version { get => version; set => version = value; }
+    }
+}
---
+++ Source/DesktopGUI/GUI.cs
@@ -0,0 +1,25 @@
+﻿using DesktopAPI;
+using System.Windows.Forms;
+
+namespace DesktopGUI
+{
+    public partial class GUI : Form
+    {
+        private API apiObj;
+
+        public GUI()
+        {
+            apiObj = new API();
+            apiObj.Title = "Desktop Application Form";
+            apiObj.Version = "V1.00.000";
+            InitializeComponent();
+
+        }
+
+        private void GUI_Load(object sender, System.EventArgs e)
+        {
+            this.Text = apiObj.Title;
+            label1.Text = apiObj.Version;
+        }
+    }
+}
